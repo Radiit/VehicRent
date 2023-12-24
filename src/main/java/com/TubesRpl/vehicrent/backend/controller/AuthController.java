e16e2cd16e2c41fe4a66ab458c93c6c4551b012f
Radiit <radityaaydin@gmail.com>
2023-12-24 16:03:25 +0700

    adding login function

--- src/main/java/com/TubesRpl/vehicrent/backend/controller/AuthController.java
+++ src/main/java/com/TubesRpl/vehicrent/backend/controller/AuthController.java
@@ -3,19 +3,30 @@ package com.TubesRpl.vehicrent.backend.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import com.TubesRpl.repository.UserRepository;
+import com.TubesRpl.vehicrent.backend.models.User;
+
 import jakarta.servlet.http.HttpSession;
 
 @Controller
 public class AuthController {
 
+    private final UserRepository userRepository;
+
+    public AuthController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
     @RequestMapping("/login")
     public String showLoginPage(Model model, HttpSession session) {
         try {
             if (session.getAttribute("user") != null) {
-                return "index";
+                return "login";
             } else {
                 return "register-form";
             }
@@ -25,27 +36,27 @@ public class AuthController {
     }
 
     @PostMapping("/login")
-    public String login(@RequestParam("email") String email,
-            @RequestParam("password") String password,
+    public String login(@RequestParam("email") String Email_User,
+            @RequestParam("password") String Password,
             Model model, HttpSession session) {
-        // Lakukan validasi login dan penanganan sesuai kebutuhan
-        // Contoh sederhana:
-        if (email.equals("example@email.com") && password.equals("password123")) {
-            session.setAttribute("user", email); // Simpan informasi user di session
-            return "redirect:/index"; // Redirect ke halaman setelah login sukses
+
+        User user = userRepository.findByEmail(Email_User);
+
+        if (user != null && user.getPassword().equals(Password)) {
+            session.setAttribute("user", user);
+            return "redirect:/login";
         } else {
-            // Tampilkan pesan error jika login gagal
             model.addAttribute("error", "Invalid credentials");
-            return "index"; // Kembali ke halaman login
+            return "login"; // Kembali ke halaman login
         }
     }
 
-    // Endpoint untuk logout jika diperlukan
-    @GetMapping("/logout")
-    public String logout(HttpSession session) {
-        session.invalidate(); // Bersihkan session
-        return "redirect:/login"; // Redirect ke halaman login setelah logout
-    }
+    // // Endpoint untuk logout jika diperlukan
+    // @GetMapping("/logout")
+    // public String logout(HttpSession session) {
+    // session.invalidate(); // Bersihkan session
+    // return "redirect:/login"; // Redirect ke halaman login setelah logout
+    // }
 
     @GetMapping("/register-form")
     public String showRegisterPage(Model model, HttpSession session) {
@@ -60,29 +71,29 @@ public class AuthController {
         }
     }
 
-    @GetMapping("/register-konfirmasi")
-    public String showRegisterKonfirmasi(Model model, HttpSession session) {
-        try {
-            if (session.getAttribute("user") != null) {
-                return "register-konfirmasi";
-            } else {
-                return "register-form";
-            }
-        } catch (Exception e) {
-            return "error-page";
-        }
-    }
+    // @GetMapping("/register-konfirmasi")
+    // public String showRegisterKonfirmasi(Model model, HttpSession session) {
+    // try {
+    // if (session.getAttribute("user") != null) {
+    // return "register-konfirmasi";
+    // } else {
+    // return "register-form";
+    // }
+    // } catch (Exception e) {
+    // return "error-page";
+    // }
+    // }
 
-    @GetMapping("/register-regent")
-    public String showRegisterRegent(Model model, HttpSession session) {
-        try {
-            if (session.getAttribute("regent") != null) {
-                return "register-regent";
-            } else {
-                return "register-form";
-            }
-        } catch (Exception e) {
-            return "error-page";
-        }
-    }
+    // @GetMapping("/register-regent")
+    // public String showRegisterRegent(Model model, HttpSession session) {
+    // try {
+    // if (session.getAttribute("regent") != null) {
+    // return "register-regent";
+    // } else {
+    // return "register-form";
+    // }
+    // } catch (Exception e) {
+    // return "error-page";
+    // }
+    // }
 }
