941a49e653138bd715f0eee1ba506130c14ecfa2
Radiit <radityaaydin@gmail.com>
2023-12-24 16:29:38 +0700

    benerin conflict

--- src/main/java/com/TubesRpl/vehicrent/backend/controller/AuthController.java
+++ src/main/java/com/TubesRpl/vehicrent/backend/controller/AuthController.java
@@ -36,16 +36,6 @@ public class AuthController {
     }
 
     @PostMapping("/login")
-<<<<<<< HEAD
-    public String login(@RequestParam("email") String email,
-            @RequestParam("password") String password,
-            Model model, HttpSession session) {
-        // Lakukan validasi login dan penanganan sesuai kebutuhan
-        // Contoh sederhana:
-        if (email.equals("example@email.com") && password.equals("password123")) {
-            session.setAttribute("user", email); // Simpan informasi user di session
-            return "redirect:/index"; // Redirect ke halaman setelah login sukses
-=======
     public String login(@RequestParam("email") String Email_User,
             @RequestParam("password") String Password,
             Model model, HttpSession session) {
@@ -55,7 +45,6 @@ public class AuthController {
         if (user != null && user.getPassword().equals(Password)) {
             session.setAttribute("user", user);
             return "redirect:/login";
->>>>>>> 11467585cc5fc934af59001d8dec1da8b0a903a8
         } else {
             model.addAttribute("error", "Invalid credentials");
             return "login"; // Kembali ke halaman login
@@ -82,72 +71,6 @@ public class AuthController {
         }
     }
 
-<<<<<<< HEAD
-    @RequestMapping("/home")
-    public String showHome(Model model, HttpSession session) {
-        try {
-            if (session.getAttribute("user") != null) {
-                return "index"; // Change to "index" or the appropriate page if the user is logged in
-            } else {
-                return "home";
-            }
-        } catch (Exception e) {
-            return "error-page";
-        }
-    }
-
-    @GetMapping("/shop")
-    public String showShop(Model model, HttpSession session) {
-        try {
-            if (session.getAttribute("user") != null) {
-                return "login"; // Change to "login" or the appropriate page if the user is logged in
-            } else {
-                return "shop";
-            }
-        } catch (Exception e) {
-            return "error-page";
-        }
-    }
-
-    @GetMapping("/history")
-    public String showHistory(Model model, HttpSession session) {
-        try {
-            if (session.getAttribute("user") != null) {
-                return "login"; // Change to "login" or the appropriate page if the user is logged in
-            } else {
-                return "history";
-            }
-        } catch (Exception e) {
-            return "error-page";
-        }
-    }
-
-    @GetMapping("/sproduct")
-    public String showSproduct(Model model, HttpSession session) {
-        try {
-            if (session.getAttribute("user") != null) {
-                return "login"; // Change to "login" or the appropriate page if the user is logged in
-            } else {
-                return "sproduct";
-            }
-        } catch (Exception e) {
-            return "error-page";
-        }
-    }
-
-    @GetMapping("/konfirmasi")
-    public String showKonfirmasi(Model model, HttpSession session) {
-        try {
-            if (session.getAttribute("user") != null) {
-                return "login"; // Change to "login" or the appropriate page if the user is logged in
-            } else {
-                return "konfirmasi";
-            }
-        } catch (Exception e) {
-            return "error-page";
-        }
-    }
-=======
     // @GetMapping("/register-konfirmasi")
     // public String showRegisterKonfirmasi(Model model, HttpSession session) {
     // try {
@@ -173,5 +96,4 @@ public class AuthController {
     // return "error-page";
     // }
     // }
->>>>>>> 11467585cc5fc934af59001d8dec1da8b0a903a8
 }
