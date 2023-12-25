ce01d9af5b8cd0df1e8cb6dacf5b0186d52b9b8e
gamerough <gamerough7@gmail.com>
2023-12-26 05:58:33 +0700

    Fetch data from controller to view history

--- src/main/java/com/TubesRpl/vehicrent/backend/controller/dashboard/HomePageController.java
+++ src/main/java/com/TubesRpl/vehicrent/backend/controller/dashboard/HomePageController.java
@@ -1,6 +1,7 @@
 package com.TubesRpl.vehicrent.backend.controller.dashboard;
 
 import java.sql.SQLException;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -9,6 +10,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.TubesRpl.repository.TransaksiRepository;
+import com.TubesRpl.vehicrent.backend.models.Transaksi;
 import com.TubesRpl.vehicrent.backend.models.User;
 
 import jakarta.servlet.http.HttpSession;
@@ -17,6 +20,9 @@ import jakarta.servlet.http.HttpSession;
 public class HomePageController {
     // @Autowired
     // private List<
+
+    @Autowired
+    private TransaksiRepository transaksiRepository;
 
     @RequestMapping("/home")
     public String homePage(Model model, HttpSession session) {
@@ -63,6 +69,26 @@ public class HomePageController {
                 User user = (User) session.getAttribute("user");
                 if (user.getRole_user().equals("Regent") || user.getRole_user().equals("Client")) {
                     return "shop"; 
+                } else if (user.getRole_user().equals("Staff")) {
+                    return "redirect:/dashboard/staff";
+                }
+            }
+
+            return "error-page"; 
+        } catch (Exception e) {
+            return "error-page"; 
+        }
+    }
+
+    @RequestMapping("/history")
+    public String history(Model model, HttpSession session) {
+        try {
+            if (session.getAttribute("user") != null) {
+                User user = (User) session.getAttribute("user");
+                if (user.getRole_user().equals("Regent") || user.getRole_user().equals("Client")) {
+                    List<Transaksi> transaksi = transaksiRepository.findAllByHiddenFalse();
+                    model.addAttribute("listTransaksi", transaksi);
+                    return "history"; 
                 } else if (user.getRole_user().equals("Staff")) {
                     return "redirect:/dashboard/staff";
                 }
